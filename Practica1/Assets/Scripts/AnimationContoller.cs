310ffecf1fe929d6e14164922b5063f11f47df4b
Nuria097 <nuria09112003@gmail.com>
2024-03-10 17:32:43 +0100

    Animation

--- Practica1/Assets/Scripts/AnimationContoller.cs
+++ Practica1/Assets/Scripts/AnimationContoller.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class AnimationContoller : MonoBehaviour
 {
+    CharacterController _characterController;
     Animator animator;
     int isWalkingHash;
     int isRunningHash;
     // Start is called before the first frame update
     void Start()
     {
+        _characterController = GetComponentInParent<CharacterController>();
         animator = GetComponent<Animator>();
         isWalkingHash = Animator.StringToHash("IsWalking");
         isRunningHash = Animator.StringToHash("IsRunning");
@@ -18,10 +20,29 @@ public class AnimationContoller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool forwardPressed;
+        Vector2 horizontalVelocity = new Vector2(_characterController.velocity.x , _characterController.velocity.z);
+        if(horizontalVelocity.magnitude == 0 ){
+            forwardPressed = false;
+            animator.SetBool(isRunningHash, false);
+        }
+        else if(horizontalVelocity.magnitude > 0 || horizontalVelocity.magnitude < 1f){
+            forwardPressed = true;
+            animator.SetBool(isRunningHash, false);
+        }
+        else{
+            animator.SetBool(isRunningHash, true);
+            forwardPressed = false;
+        }
+        
+        
+
         bool IsRunning = animator.GetBool(isRunningHash);
         bool IsWalking = animator.GetBool(isWalkingHash);
-        bool forwardPressed = Input.GetKey("w");
+        bool IsJumping = animator.GetBool("IsJumping");
+        //bool forwardPressed = Input.GetKey("w"); //wasd
         bool runPressed = Input.GetKey("left shift");
+        bool jumpPressed = Input.GetKey("space");
 
         if(!IsWalking && forwardPressed){
             animator.SetBool(isWalkingHash, true);
@@ -35,8 +56,10 @@ public class AnimationContoller : MonoBehaviour
             animator.SetBool(isRunningHash, true);
         }
 
-        if(IsRunning && (!forwardPressed || !runPressed)){
-            animator.SetBool(isRunningHash, false);
-        }
+        //if(IsRunning && (!forwardPressed || !runPressed)){
+          //  animator.SetBool(isRunningHash, false);
+        //}
+
+        //if()
     }
 }
